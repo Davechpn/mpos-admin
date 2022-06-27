2e847253c237588153d34cd89d6a8d13b3a18045
David Chipundo <chipundodavid@gmail.com>
2022-06-27 09:25:24 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-blue-100">
+     tailwind
+     <div className="text-2xl bg-sky-50 font-bold underline">
+      Hello world!
+    </div>
+
     </div>
   );
 }
